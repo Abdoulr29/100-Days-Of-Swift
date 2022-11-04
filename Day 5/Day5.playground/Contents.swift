8c117b06359ed7d3fbc97e440678d5b17e68fd92
Developer <abdoulr29@gmail.com>
2022-11-04 19:47:02 +0200

    Default parameters

--- Day 5/Day5.playground/Contents.swift
+++ Day 5/Day5.playground/Contents.swift
@@ -24,3 +24,14 @@ func greet(_ person: String) {
 }
 
 greet("Adam")
+
+func greet(_ person: String, nicely: Bool = true) {
+    if nicely == true {
+        print("Hello, \(person)!")
+    } else {
+        print("Oh no, it's \(person) again...")
+    }
+}
+
+greet("Taylor")
+greet("Taylor", nicely: false)
