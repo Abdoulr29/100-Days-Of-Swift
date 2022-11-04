ef94986f98305fb036e104c319ef7eab0b65029d
Developer <abdoulr29@gmail.com>
2022-11-04 19:25:04 +0200

    Exiting multiple loops

--- Day 4/Day4.playground/Contents.swift
+++ Day 4/Day4.playground/Contents.swift
@@ -26,3 +26,16 @@ repeat {
     print("\(x)")
 
 } while x<10
+
+
+outerLoop: for i in 1...10 {
+    for j in 1...10 {
+        let product = i * j
+        print ("\(i) * \(j) is \(product)")
+
+        if product == 50 {
+            print("It's a bullseye!")
+            break outerLoop
+        }
+    }
+}
