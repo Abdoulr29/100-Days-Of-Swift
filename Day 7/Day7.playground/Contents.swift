e7590bda52288af5d318e92fcf98224bd9cce129
Developer <abdoulr29@gmail.com>
2022-11-07 17:47:21 +0200

    Shorthand parameter names

--- Day 7/Day7.playground/Contents.swift
+++ Day 7/Day7.playground/Contents.swift
@@ -1,8 +1,5 @@
 import UIKit
 
-
-
-
 func travel(action: (String) -> String) {
     print("I'm getting ready to go.")
     let description = action("London")
@@ -10,7 +7,10 @@ func travel(action: (String) -> String) {
     print("I arrived!")
 }
 
-
 travel { (place: String) -> String in
+    return "I'm going to \(place) in my car"
+}
+
+travel { place -> String in
     return "I'm going to \(place) in my car"
 }
