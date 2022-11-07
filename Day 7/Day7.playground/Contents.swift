a9dc865087a5fdb6248de4b34ced5a77cdf9af22
Developer <abdoulr29@gmail.com>
2022-11-07 17:46:19 +0200

    Using closures as parameters when they return values

--- Day 7/Day7.playground/Contents.swift
+++ Day 7/Day7.playground/Contents.swift
@@ -1,11 +1,16 @@
 import UIKit
 
-func travel(action: (String) -> Void) {
+
+
+
+func travel(action: (String) -> String) {
     print("I'm getting ready to go.")
-    action("London")
+    let description = action("London")
+    print(description)
     print("I arrived!")
 }
 
-travel { (place: String) in
-    print("I'm going to \(place) in my car")
+
+travel { (place: String) -> String in
+    return "I'm going to \(place) in my car"
 }
