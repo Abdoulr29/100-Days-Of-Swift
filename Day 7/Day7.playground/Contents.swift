42f58280c550d046bfbde6bc358969ae1c9a24b5
Developer <abdoulr29@gmail.com>
2022-11-07 17:48:18 +0200

    Closures with multiple parameters

--- Day 7/Day7.playground/Contents.swift
+++ Day 7/Day7.playground/Contents.swift
@@ -1,16 +1,12 @@
 import UIKit
 
-func travel(action: (String) -> String) {
+func travel(action: (String, Int) -> String) {
     print("I'm getting ready to go.")
-    let description = action("London")
+    let description = action("London", 60)
     print(description)
     print("I arrived!")
 }
 
-travel { (place: String) -> String in
-    return "I'm going to \(place) in my car"
-}
-
-travel { place -> String in
-    return "I'm going to \(place) in my car"
+travel {
+    "I'm going to \($0) at \($1) miles per hour."
 }
