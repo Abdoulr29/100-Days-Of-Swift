a0ea7aa852a57a5df9a1a05d03dc2381d246aeea
Developer <abdoulr29@gmail.com>
2022-11-07 17:50:40 +0200

    Returning closures from functions

--- Day 7/Day7.playground/Contents.swift
+++ Day 7/Day7.playground/Contents.swift
@@ -1,12 +1,13 @@
 import UIKit
 
-func travel(action: (String, Int) -> String) {
-    print("I'm getting ready to go.")
-    let description = action("London", 60)
-    print(description)
-    print("I arrived!")
+func travel() -> (String) -> Void {
+    return {
+        print("I'm going to \($0)")
+    }
 }
 
-travel {
-    "I'm going to \($0) at \($1) miles per hour."
-}
+let result = travel()
+result("London")
+
+let result2 = travel()("London")
+
