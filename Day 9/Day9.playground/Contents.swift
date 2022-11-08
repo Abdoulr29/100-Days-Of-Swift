463bc82030377ae86c995918df05fdb7f69c50c2
Developer <abdoulr29@gmail.com>
2022-11-08 21:18:43 +0200

    Static properties and methods

--- Day 9/Day9.playground/Contents.swift
+++ Day 9/Day9.playground/Contents.swift
@@ -19,3 +19,19 @@ var ed = Person(name: "Ed")
 ed.familyTree
 
 
+struct Student {
+    static var classSize = 0
+    var name: String
+
+    init(name: String) {
+        self.name = name
+        Student.classSize += 1
+    }
+}
+
+let eddy = Student(name: "Ed")
+let taylor = Student(name: "Taylor")
+
+print(Student.classSize)
+
+
