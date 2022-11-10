59ef97a86a5ad4adb1f25f0fcc9a668f4e11c834
Developer <abdoulr29@gmail.com>
2022-11-10 23:57:50 +0200

    Protocols

---
+++ Day 11/Day11.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+protocol Identifiable {
+    var id: String { get set }
+}
+
+struct User: Identifiable {
+    var id: String
+}
+
+func displayID(thing: Identifiable) {
+    print("My ID is \(thing.id)")
+}
