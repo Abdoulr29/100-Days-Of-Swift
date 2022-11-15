95b19bf3347e5df74cb0b7106a9b8674dd183a9e
Developer <abdoulr29@gmail.com>
2022-11-16 00:00:31 +0200

    Consolidation Types of Data

--- Day 13/Day13.playground/Contents.swift
+++ Day 13/Day13.playground/Contents.swift
@@ -1,6 +1,29 @@
 import UIKit
 
-var name = "testUser"
-name = "user"
-
 let test = "test"
+
+var name: String
+name = "Tim McGraw"
+
+var age: Int
+age = 25
+
+var latitude: Double
+latitude = 36.166667
+
+var longitude: Float
+longitude = -86.783333
+longitude = -186.783333
+longitude = -1286.783333
+longitude = -12386.783333
+longitude = -123486.783333
+longitude = -1234586.783333
+
+var stayOutTooLate: Bool
+stayOutTooLate = true
+
+var nothingInBrain: Bool
+nothingInBrain = true
+
+var missABeat: Bool
+missABeat = false
