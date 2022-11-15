f79635f4de3cddfad477a92af6fab590c6d5cbe9
Developer <abdoulr29@gmail.com>
2022-11-16 00:15:38 +0200

    Consolidation Operators

--- Day 13/Day13.playground/Contents.swift
+++ Day 13/Day13.playground/Contents.swift
@@ -19,11 +19,40 @@ longitude = -12386.783333
 longitude = -123486.783333
 longitude = -1234586.783333
 
-var stayOutTooLate: Bool
-stayOutTooLate = true
+
 
 var nothingInBrain: Bool
 nothingInBrain = true
 
 var missABeat: Bool
 missABeat = false
+
+var a = 10
+a = a + 1
+a = a - 1
+a = a * a
+
+var b = 10
+b += 10
+b -= 10
+
+var name1 = "Tim McGraw"
+var name2 = "Romeo"
+var both = name1 + " and " + name2
+
+
+var x = 1.1
+var y = 2.2
+var k = x + y
+
+x > 3
+x >= 3
+x > 4
+x < 4
+
+var userName = "Tim McGraw"
+userName == "Tim McGraw"
+
+var stayOutTooLate = true
+stayOutTooLate
+!stayOutTooLate
