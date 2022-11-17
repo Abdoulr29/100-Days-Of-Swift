56db69e891ce46ad8d55808adf903b5118ddaf68
Developer <abdoulr29@gmail.com>
2022-11-17 23:52:23 +0200

    Consolidation Switch/case

--- Day 13/Day13.playground/Contents.swift
+++ Day 13/Day13.playground/Contents.swift
@@ -123,10 +123,43 @@ while true {
 }
 
 
-for song in songz {
+for song in songs {
     if song == "You Belong with Me" {
         continue
     }
 
     print("My favorite song is \(song)")
+}
+
+let liveAlbums = 2
+
+switch liveAlbums {
+case 0:
+    print("You're just starting out")
+
+case 1:
+    print("You just released iTunes Live From SoHo")
+
+case 2:
+    print("You just released Speak Now World Tour")
+
+default:
+    print("Have you done something new?")
+}
+
+
+let studioAlbums = 5
+
+switch studioAlbums {
+case 0...1:
+    print("You're just starting out")
+
+case 2...3:
+    print("You're a rising star")
+
+case 4...7:
+    print("You're world famous!")
+
+default:
+    print("Have you done something new?")
 }
