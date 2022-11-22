eb3ebed773e0befc96483fee25cba685c3489c97
Developer <abdoulr29@gmail.com>
2022-11-22 23:53:30 +0200

    Consolidation optional chaining

--- Day 14/Day14.playground/Contents.swift
+++ Day 14/Day14.playground/Contents.swift
@@ -45,3 +45,21 @@ func getHaterStatus(weather: String) -> String? {
 
 var status: String
 status = getHaterStatus(weather: "rainy")!
+
+func albumReleased(year: Int) -> String? {
+    switch year {
+    case 2006: return "Taylor Swift"
+    case 2008: return "Fearless"
+    case 2010: return "Speak Now"
+    case 2012: return "Red"
+    case 2014: return "1989"
+    default: return nil
+    }
+}
+
+
+let album = albumReleased(year: 2006)?.uppercased()
+print("The album is \(album)")
+
+let albumm = albumReleased(year: 2006) ?? "unknown"
+print("The album is \(albumm)")
