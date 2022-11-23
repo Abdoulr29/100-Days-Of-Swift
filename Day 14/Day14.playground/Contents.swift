10e2c2a6554f8718fab6da99965f528bd8b011ac
Developer <abdoulr29@gmail.com>
2022-11-23 23:21:52 +0200

    Consolidation Enumerations

--- Day 14/Day14.playground/Contents.swift
+++ Day 14/Day14.playground/Contents.swift
@@ -63,3 +63,57 @@ print("The album is \(album)")
 
 let albumm = albumReleased(year: 2006) ?? "unknown"
 print("The album is \(albumm)")
+
+
+
+//enum WeatherType {
+//    case sun
+//    case cloud
+//    case rain
+//    case wind
+//    case snow
+//}
+//
+//func getHaterStatus(weather: WeatherType) -> String? {
+//    if weather == .sun {
+//        return nil
+//    } else {
+//        return "Hate"
+//    }
+//}
+//
+//getHaterStatus(weather: .cloud)
+//
+//func getHaterStatus(weather: WeatherType) -> String? {
+//    switch weather {
+//    case .sun:
+//        return nil
+//    case .cloud, .wind:
+//        return "dislike"
+//    case .rain:
+//        return "hate"
+//    }
+//}
+//
+enum WeatherType {
+    case sun
+    case cloud
+    case rain
+    case wind(speed: Int)
+    case snow
+}
+
+func getHaterStatus(weather: WeatherType) -> String? {
+    switch weather {
+    case .sun:
+        return nil
+    case .wind(let speed) where speed < 10:
+        return "meh"
+    case .cloud, .wind:
+        return "dislike"
+    case .rain, .snow:
+        return "hate"
+    }
+}
+
+getHaterStatus(weather: WeatherType.wind(speed: 5))
