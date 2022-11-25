7472a0fca44cb7b5ec267a3fcdb95aa8318b3089
Developer <abdoulr29@gmail.com>
2022-11-25 23:43:15 +0200

    Consolidation Struct

--- Day 14/Day14.playground/Contents.swift
+++ Day 14/Day14.playground/Contents.swift
@@ -117,3 +117,21 @@ func getHaterStatus(weather: WeatherType) -> String? {
 }
 
 getHaterStatus(weather: WeatherType.wind(speed: 5))
+
+struct Person {
+    var clothes: String
+    var shoes: String
+    
+    func describe() {
+            print("I like wearing \(clothes) with \(shoes)")
+        }
+}
+
+let taylor = Person(clothes: "T-shirts", shoes: "sneakers")
+let other = Person(clothes: "short skirts", shoes: "high heels")
+
+var taylorCopy = taylor
+taylorCopy.shoes = "flip flops"
+
+print(taylor)
+print(taylorCopy)
