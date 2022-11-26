1b2953d41d3153964de53107f2124eb399f9593c
Developer <abdoulr29@gmail.com>
2022-11-26 17:14:25 +0200

    Consolidation Classes

--- Day 14/Day14.playground/Contents.swift
+++ Day 14/Day14.playground/Contents.swift
@@ -135,3 +135,40 @@ taylorCopy.shoes = "flip flops"
 
 print(taylor)
 print(taylorCopy)
+
+
+class Singer {
+    var name: String
+    var age: Int
+
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+
+    func sing() {
+        print("La la la la")
+    }
+}
+
+class CountrySinger: Singer {
+    override func sing() {
+        print("Trucks, guitars, and liquor")
+    }
+}
+
+var singer = CountrySinger(name: "Taylor", age: 25)
+singer.sing()
+
+class HeavyMetalSinger: Singer {
+    var noiseLevel: Int
+
+    init(name: String, age: Int, noiseLevel: Int) {
+        self.noiseLevel = noiseLevel
+        super.init(name: name, age: age)
+    }
+
+    override func sing() {
+        print("Grrrrr rargh rargh rarrrrgh!")
+    }
+}
